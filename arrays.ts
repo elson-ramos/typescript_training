3feac2561d4c2d3461f1841b273cf9bfa9e355ee
elson_ramos <ramoselson1234@gmail.com>
2026-01-31 18:48:53 -0300

    adicionar os arquivos já existentes

---
+++ arrays.ts
@@ -0,0 +1,9 @@
+// primeira forma: definindo os tipos dos elementos dentro do array
+let esseArrayEDeStrings: string[] = ['madeira', 'pedra', 'ovelha', 'argila', 'feno'];
+console.log(esseArrayEDeStrings);
+
+// segunda forma: array generico
+let esseArrayEDeNumeros: Array<string> = ['um', 'dois', 'tres', 'quatro', 'cinco'];
+console.log(esseArrayEDeNumeros)
+
+// o dev (ou a empresa) escolhe qual é melhor, não há nenhuma diferença entre os dois tipos
