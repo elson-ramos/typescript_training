f05ae5d05fbfee1e8187ecd251f74ec4a6657764
elson_ramos <ramoselson1234@gmail.com>
2026-01-31 19:57:40 -0300

    mudança do nome do arquivo

--- como usar o commit para o github.ts
+++ como usar o commit para o github.ts
@@ -1,3 +1,9 @@
 // iniciar o repositorio na pasta
 // comando: git init
-// para dar commit para arquivos ja existentes: **git commit -am "mensagem dizendo o que fez"**
+// para dar commit para arquivos ja existentes: **git commit -(opcoes) "mensagem dizendo o que fez"**
+
+// -m: padrão, usar esse
+
+// -a: All, pega tudo que foi modificado ou deletado e coloca no pacote antes do commit.
+
+// -am: junção dos dois acima
