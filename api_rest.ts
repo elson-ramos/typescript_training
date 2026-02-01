ed554d3cff1dfee23d7692acda54e214cdc8558d
elson_ramos <ramoselson1234@gmail.com>
2026-02-01 10:05:40 -0300

    adicao da parte de json

---
+++ api_rest.ts
@@ -0,0 +1,15 @@
+// REST: Representational State Transfer
+
+// modelo de arquitetura que utiliza unicamente o protocolo HTTP
+
+// baixo acoplamento
+
+
+
+// RESTful: implementação dos padrões definidos pela arquitetura REST.
+
+// aplicação backend, independente de qualquer aplicação frontend.
+
+// utiliza o HTTP
+
+// troca de mensagens entre o front
