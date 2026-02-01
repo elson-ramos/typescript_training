8c74ac300f617bb0e813c08ad5d8b3b2c211c3fe
elson_ramos <ramoselson1234@gmail.com>
2026-01-31 21:35:00 -0300

    a

--- tipos_de_variaveis.ts
+++ tipos_de_variaveis.ts
@@ -1,3 +1,16 @@
+//          boolean
+
 let ativacao:boolean = false;
 ativacao = true;
 console.log(ativacao);
+
+// exemplo:
+let temVirus:boolean = true;
+
+if (!a) {
+    console.log("nao ta tendo");
+}else{
+    console.log("ta tendo");
+}
+
+//          
