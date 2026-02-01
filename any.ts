6178f68e0ac3f367ea95b52ad4cf8a1007556078
elson_ramos <ramoselson1234@gmail.com>
2026-02-01 08:12:17 -0300

    add any type

---
+++ any.ts
@@ -0,0 +1,10 @@
+// usado quando se migra um projeto de JS para TS.
+// ou
+// quando recebe-se o valor de uma fonte, mas não se sabe o tipo
+
+// detalhe: não tipar tudo com any.
+
+let qualquer: any;
+qualquer = 'armazeno qualquer coisa'
+qualquer = 4;
+qualquer = true;
