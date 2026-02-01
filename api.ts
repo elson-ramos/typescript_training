38f79bef42f9f31f981f6f4169481ceb6cd59c05
elson_ramos <ramoselson1234@gmail.com>
2026-02-01 08:24:03 -0300

    add api meaning

---
+++ api.ts
@@ -0,0 +1,5 @@
+// API: Application Programming Interface
+
+// Permite que um software se comunique com outro
+
+// Atualmente, significa serviços web (como http) que troca informações entre o back e o front.
