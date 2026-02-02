167e5e377a35082254d87efeed91c3ae000e4938
elson_ramos <ramoselson1234@gmail.com>
2026-02-02 15:08:46 -0300

    inclusão da função status do servidor

--- projeto_frontEnd/script.js
+++ projeto_frontEnd/script.js
@@ -1,1 +1,21 @@
-console.log("working 👍👍")
+async function statusServidor(){
+    const URL = "http://localhost:3000";
+  
+    try {
+      const response = await fetch(URL, {
+        method: "GET",
+        mode: 'cors',
+        headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'text/plain',
+        }
+      });
+  
+      const resposta = await response.text();
+      console.log(resposta);
+  
+    } catch (error) {
+      console.error("Ocorreu um erro:", error);
+    }
+  }
+  statusServidor();
