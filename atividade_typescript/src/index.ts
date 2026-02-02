8d2e04e4fcfdc827570c62cc142becd64b3345f9
elson_ramos <ramoselson1234@gmail.com>
2026-02-02 06:47:49 -0300

    implementação de funções e fluxograma (png)

--- atividade_typescript/src/index.ts
+++ atividade_typescript/src/index.ts
@@ -24,3 +24,15 @@ console.log(repositorio.listarTodos()); //listagem de todos os registros
 console.log("----- Busca pelo ID -----");
 const buscaPessoa = repositorio.buscarPorId(4);
 console.log(buscaPessoa);
+
+// atualização de registros
+console.log("----- Atualizar Registro -----");
+buscaPessoa.email = "novo_email@teste.com.br";
+buscaPessoa.anoNacimento = 2000;
+repositorio.atualizar(buscaPessoa.id, buscaPessoa);  // com o id do buscaPessoa, alterou-se o email e a data de nascimento
+console.log(repositorio.listarTodos());
+
+// excluir registro
+console.log("----- Excluir Registro -----");
+repositorio.excluir(2);
+console.log(repositorio.listarTodos());
