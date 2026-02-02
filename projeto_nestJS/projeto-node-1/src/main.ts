fd2da05ac30c15725304fa2fd9967d856f511b62
elson_ramos <ramoselson1234@gmail.com>
2026-02-02 15:02:37 -0300

    inclusão do projeto FrontEnd

--- projeto_nestJS/projeto-node-1/src/main.ts
+++ projeto_nestJS/projeto-node-1/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(
+    {
+      allowedHeaders: '*',
+      origin : '*',
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }
+  )
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
