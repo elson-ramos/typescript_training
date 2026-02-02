1e34eede26b57749e965f0858d44d988ac7c6494
elson_ramos <ramoselson1234@gmail.com>
2026-02-02 13:19:25 -0300

    complementos para o projeto nestJS

--- projeto_nestJS/projeto-node-1/src/app.controller.ts
+++ projeto_nestJS/projeto-node-1/src/app.controller.ts
@@ -1,12 +1,60 @@
-import { Controller, Get } from '@nestjs/common';
+// controller: criação de rotas
+
+import { 
+  Controller, 
+  Get, 
+  Post, 
+  Put, 
+  Delete, 
+  Query, 
+  Body,
+  Param,  
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Cliente } from './modelo/cliente';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor(private readonly appService: AppService) {} // recebe uma instância da classe service
+
+  @Get() //Exemplo: http://localhost:3000
+  getStatus(): string {
+    return "O node está rodando: " + new Date();
+  }
+  // adição da url acontece aqui
+  @Get("/clientes") //Exemplo: http://localhost:3000/clientes
+  listarTodosClicentes():Array<Cliente> {
+    console.log("Entrou no método: listarTodosClicentes "+ new Date());  // retorna apenas o array de clientes
+
+    return this.appService.listarTodos();
+  }
+
+  @Get("/cliente") //Exemplo: http://localhost:3000?id=1     retorna o valor pela URL
+  public buscarPorId(@Query('id') id:number): Cliente | undefined{  // @Querry = obtenção do valor do ID
+    console.log("Entrou no método: buscarPorId "+ new Date());
+
+    return this.appService.buscarPorId(id);
+  }
+
+  @Post() // acionadas apenas por requisições post
+  public salvar(@Body() cliente: Cliente):Cliente{  // quando não se informa um URL,
+    console.log("Entrou no método: salvar");        // indica que é a rota raiz do método HTTP Post
+
+    return this.appService.salvar(cliente);
+  }
+
+  @Put(':id')
+  public alterar(@Param('id') id: number, @Body() cliente: Cliente ) : Cliente{
+    console.log("Entrou no método: alterar "+ new Date());
+      
+    return this.appService.atualizar(id, cliente);
+  }
+
+  @Delete(':id')
+  public excluir(@Param('id') id: number){
+    console.log("Entrou no método: delete "+ new Date());
+
+    this.appService.excluir(id);
   }
 }
--- projeto_nestJS/projeto-node-1/src/app.service.ts
+++ projeto_nestJS/projeto-node-1/src/app.service.ts
@@ -1,8 +1,89 @@
 import { Injectable } from '@nestjs/common';
+import { Cliente } from './modelo/cliente';
 
+// decorator, recurso do JS.
+// responsável por criar uma instância únida dessa classe
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  //Array de Clientes
+  private listaClientes:Array<Cliente> = []; // declaração da lista de clientes
+
+  constructor(){  // objetos do tipo "Cliente"
+    const client1 = new Cliente(1,'Alice Almeida','alice@teste.com.br', 2010);
+    const client2 = new Cliente(2,'Carlos Alberto','carlos@teste.com.br', 2007);
+    const client3 = new Cliente(3,'Alice Almeida','teste1@gmail.com', 2003);
+
+    //Adiciona os objetos ao array
+    this.listaClientes.push(client1, client2, client3);
+
+    console.log(this.listaClientes); // imprime no terminal o conteúdo do array
+  }
+
+  public listarTodos():Array<Cliente>{  // retorna apenas o array de clientes
+    return this.listaClientes;
+  } 
+
+  public buscarPorId(id:number): Cliente | undefined {  // recebe o numero do identificador e retorna o registro com o identificador
+    return this.listaClientes.find(
+        cliente => cliente.id == id
+    );
+  }
+
+  public salvar(cliente:Cliente): Cliente{  // inclui novos registros na lista de clientes
+
+    console.log(cliente);
+
+    //Cria um novo registro
+    const incluirCliente = new Cliente(
+      this.obterProximoId(), // pega o ultimo registro e incrementa em 1, e atribui a esse índice o novo registro.
+      cliente.nome,
+      cliente.email,
+      cliente.anoNacimento
+    );
+
+    this.listaClientes.push(incluirCliente);
+    console.log(incluirCliente);
+    return incluirCliente;
+  }
+
+  public atualizar(id:number, cliente:Cliente): Cliente{
+    //Encontra o índice do registro que será atualizado
+    let indice = this.listaClientes.findIndex(
+      cliente => cliente.id == id
+    );
+
+    //Atualiza o registro
+    this.listaClientes[indice] = cliente;
+    return cliente;
+  }
+
+  public excluir(id:number): void{
+    //Encontra o índice do registro que será removido
+    let indice = this.listaClientes.findIndex(
+      cliente => cliente.id == id
+    );
+
+    //Remove o registo do array
+    this.listaClientes.splice(indice, 1);
+  }
+
+  //Apenas para simular a chave incremental do banco de dados
+  //Retorna o próximo valor do identificado para que não 
+  //haja duplicidade de identificadores
+  public obterProximoId(): number{
+      
+    if(this.listaClientes.length == 0){
+      return 1
+    }else{
+      //Obtém o último registro do array
+      let ultimoRegistro = this.listaClientes[
+          this.listaClientes.length - 1
+      ];
+
+      //Incrementa o identificador 
+      //para ser usado um novo registro
+      return ultimoRegistro.id + 1;
+    }
   }
 }
